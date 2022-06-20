44897157fe86f70e85ba15902e22211b69b01f1d
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-06-20 08:42:52 -0700

    Lesson_11 classwork_07 added

--- lesson_11/classwork_04.py
+++ lesson_11/classwork_04.py
@@ -2,3 +2,25 @@
 7. Создать программу, позволяющую осуществлять поиск по имени или возрасту, оба параметра вводятся с клавиатуры.
 """
 
+import sqlite3
+
+
+def select_user(firstname="", age=0):
+    with sqlite3.connect("my_database.sqlite3") as session:
+        cursor = session.cursor()
+        cursor.execute(
+            """
+           SELECT *
+           FROM user
+           WHERE user.firstname = ? or age = ?;
+           """,
+            (firstname, age)
+        )
+        session.commit()
+        return cursor.fetchall()
+
+
+if __name__ == "__main__":
+    name_1 = input("Enter name: ")
+    age_1 = int(input("Enter age: "))
+    print(select_user(name_1, age_1))
